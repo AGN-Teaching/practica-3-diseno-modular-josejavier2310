16a3642c18482283e721f6e3f4d314f76e95a470
josejavier2310 <jose.j.perez@cua.uam.mx>
2025-04-04 19:40:13 -0600

    Update analisis_texto.py

--- analisis_texto.py
+++ analisis_texto.py
@@ -4,21 +4,14 @@ de palabras y de oraciones contenidas en un texto
 que se recibe como entrada.
 """
 
-
 def contar_oraciones(texto):
     """Cuenta y devuelve el número de oraciones en texto."""
-    
-    # Completar
-    
+    return len(re.findall(r'[.!?;:]', texto))    
 
 def contar_palabras(texto):
     """Cuenta y devuelve el número de palabras en texto."""
-    
-    # Completar
-
+    return len(re.findall(r'\b\w+\b', texto))
 
 def contar_silabas(texto):
     """Cuenta y devuelve el número de sílabas en texto."""
-    
-    # Completar
-
+    return len(re.findall(r'[aeiouáéíóúü]', texto, re.IGNORECASE))
