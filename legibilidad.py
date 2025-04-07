950bc0fac20c22cba8e41b462f02409301631ed3
josejavier2310 <jose.j.perez@cua.uam.mx>
2025-04-07 12:13:51 -0600

    Update legibilidad.py

--- legibilidad.py
+++ legibilidad.py
@@ -6,25 +6,26 @@ import analisis_texto as at
 
 def indice_legibilidad_VG(texto):
     """Calcula y devuelve el índice de Velázquez Gaytán del texto de entrada."""
-    oraciones = contar_oraciones(texto)
-    palabras = contar_palabras(texto)
-    silabas = contar_silabas(texto)
+    oraciones = at.contar_oraciones(texto)
+    palabras = at.contar_palabras(texto)
+    silabas = at.contar_silabas(texto)
 
-if palabras == 0:
-    return oraciones, palabras, silabas, 0
+    if palabras == 0 or oraciones == 0:
+        return oraciones, palabras, silabas, 0
 
-L = 206.84 - 1.02 (palabras / oraciones) - 60 (sílabas / palabras)
-    return oraciones, palabras, silabas
+    L = 206.84 - 1.02 * (palabras / oraciones) - 60 * (silabas / palabras)
+    return oraciones, palabras, silabas, L
 
 def nivel_legibilidad_VG(indice):
     """Decide y devuelve el nivel de legibilidad de un texto de acuerdo con el índice de Velázquez Gaytán."""
-if indice >= 80: 
-    return "Muy fácil" 
-elif indice >= 60: 
-    return "Fácil" 
-elif indice >= 40: 
-    return "Intermedio" 
-elif indice >= 20: 
-    return "Difícil" 
-else: 
-    return "Muy difícil"
+    if indice >= 80: 
+        return "Muy fácil" 
+    elif indice >= 60: 
+        return "Fácil" 
+    elif indice >= 40: 
+        return "Intermedio" 
+    elif indice >= 20: 
+        return "Difícil" 
+    else: 
+        return "Muy difícil"
+
